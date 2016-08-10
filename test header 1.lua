a1d4dabe23392f12387af9f530be2ad8fa3e1ff9
juliansader <julian.sader@gmail.com>
2016-08-10 09:56:43 +0200

    Create test header 1.lua

---
+++ test header 1.lua
@@ -0,0 +1,17 @@
+--[[
+Description: js_Notation - Set displayed length of selected notes to custom value.lua
+
+About:
+  #Description
+  Sets the displayed length of selected notes (in the MIDI editor's notation view) to a 
+  value that the user can specify in a popup window.
+]]
+
+--[[
+Author: juliansader
+Website:
+  Forum Thread: http://forum.cockos.com/showthread.php?t=172782&page=25
+Version: 1.0
+Changelog:
+  + Initial beta release
+]]
