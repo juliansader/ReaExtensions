f76451f03406e480b4cfadc437d103c006b9021d
juliansader <julian.sader@gmail.com>
2016-08-11 01:26:51 +0200

    Update test header 3.lua

--- Envelopes/test header 3.lua
+++ Envelopes/test header 3.lua
@@ -2,8 +2,7 @@
 Description: This goes in the package browser.
 About:
   # Description
-  This script does awesome things that will make you save 1 hour of repetitive work each day!
-
+  This script does awesome things that will make you save 1 hour of repetitive work each day.
   # Instructions
   Clap in your hands twice and do a backflip to make this script works.
 --]]
