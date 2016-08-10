b341008be95a4e4404e1a1894459132226b8f7cc
juliansader <julian.sader@gmail.com>
2016-08-11 01:23:31 +0200

    Create test header 3.lua

---
+++ Envelopes/test header 3.lua
@@ -0,0 +1,10 @@
+--[[
+Description: This goes in the package browser.
+About:
+  # Description
+  This script does awesome things that will make you save 1 hour of repetitive work each day!
+
+  # Instructions
+  Clap in your hands twice and do a backflip to make this script works.
+--]]
+-- version 1.0
