35e6dbd8fadf992c4bd8b6bd52c15f6f5bd4b356
juliansader <julian.sader@gmail.com>
2016-08-11 02:47:48 +0200

    Update test header 1.lua

--- Envelopes/test header 1.lua
+++ Envelopes/test header 1.lua
@@ -1,9 +1,9 @@
 --[[
 Description: js_Notation - Set displayed length of selected notes to custom value.lua
+Version: 1.2
 About:
-  #Description
+  # Description
   Sets the displayed length of selected notes (in the MIDI editor's notation view) to a 
   value that the user can specify in a popup window.
 Author: juliansader
-Version: 1.1
 ]]
