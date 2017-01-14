592d30261a5297a03e10caa5b40631f068507d32
juliansader <julian.sader@gmail.com>
2017-01-14 08:26:00 +0200

    Create test header 6.lua

---
+++ Envelopes/test header 6.lua
@@ -0,0 +1,14 @@
+--[[
+ReaScript name: Script: js_LFO Tool (MIDI editor version, insert CCs in time selection in lane under mouse).lua
+Version: v2.00
+Author: juliansader    
+]]
+
+--[[
+  Changelog:
+  v2.00 (2017-01-15)
+    + Much faster execution in large takes with hundreds aof thousands of events.
+    + Keyboard shortcuts "a", "c" and "r" to switch GUI views.
+    + LFO can be applied to existing events - including velocities - instead of inserting new CCs.
+    + Requires REAPER v5.32 or later.  
+]]
